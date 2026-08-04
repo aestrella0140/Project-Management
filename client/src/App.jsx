7f224015f612714b42883722f76f0aad0d04c0fc
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2026-08-04 16:38:46 -0700

    initial setup for apollo client on the main.jsx and app.jsx using routerProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,48 @@
-import { useState } from 'react'
+import { Outlet } from "react-router-dom";
 
-import './App.css'
+import {
+  ApolloClient,
+  inMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+
+import { setContext, SetContext } from "@apollo/client/link/context";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+const httpLink = createHttpLink({
+  URI: import .mata.env.VITE_GRAPH_URI,
+});
+
+const authlink = setContext((_, { headers}) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authlink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      
-    </>
+    <ApolloProvider client={client}>
+      <div>
+        <Outlet />
+      </div>
+    </ApolloProvider>
   )
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { Children } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom/dist";
+
+const router = createBrowserRouter([
+  {
+    Path: "/",
+    element: <App />,
+    Children: [
+      {
+
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <RouterProvider router={router} />,
+);
